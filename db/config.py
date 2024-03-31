9ceb60d6d9da4f368198d01ee292be04699b3d44
Anees <anees@Aneess-MacBook-Pro-2.local>
2024-03-31 23:33:38 +0530

    cleanup

--- db/config.py
+++ db/config.py
@@ -9,33 +9,3 @@ Session = sessionmaker(bind=engine)
 
 # Create a session
 db_session = Session()
-
-# import psycopg
-
-# def setup_db():
-#   conn = _setup_connection()
-#   _setup_tables(conn)
-  
-  
-# # Private methods
-# def _setup_connection():
-#   conn = psycopg.connect(dbname = "mydatabase", 
-#                       user = "myuser", 
-#                       host= 'localhost',
-#                       password = "mypassword",
-#                       port = 5432)
-#   return conn
-
-# def _setup_tables(conn):
-#   cur = conn.cursor()
-#   cur.execute("""CREATE TABLE datacamp_courses(
-#               course_id SERIAL PRIMARY KEY,
-#               course_name VARCHAR (50) UNIQUE NOT NULL,
-#               course_instructor VARCHAR (100) NOT NULL,
-#               topic VARCHAR (20) NOT NULL);
-#               """)
-#   conn.commit()
-#   cur.close()
-#   conn.close()
-
-  

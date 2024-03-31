33e04204ecc07a4919c9f333a56c8c99c5c65415
Anees <anees@Aneess-MacBook-Pro-2.local>
2024-03-31 18:34:55 +0530

    Authenticator and fetch email logic

---
+++ fetch_emails.py
@@ -0,0 +1,53 @@
+import base64
+import email
+import json
+import authenticator
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.oauth2.credentials import Credentials
+import os
+
+
+def list_messages(service, user_id="me"):
+  try:
+    results = service.users().messages().list(userId=user_id).execute()
+    messages = results.get("messages", [])
+    return messages
+  except Exception as e:
+    print('An error occurred:', e)
+    return []
+    
+def get_message(service, message_id, user_id='me'):
+    try:
+        message = service.users().messages().get(userId=user_id, id=message_id, format='raw').execute()
+        return message
+    except Exception as e:
+        print('An error occurred:', e)
+        return None
+
+def decode_message(message):
+    try:
+        msg_str = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
+        mime_msg = email.message_from_bytes(msg_str)
+        return mime_msg
+    except Exception as e:
+        print('An error occurred:', e)
+        return None
+
+if __name__ == '__main__':
+  # Authenticate and get Gmail service
+  service = authenticator.authenticate_and_get_service()
+  messages = list_messages(service)
+  for message in messages:
+    message_id = message['id']
+    msg = get_message(service, message_id)
+    # print(msg)
+    decoded_msg = decode_message(msg)
+    # print(decoded_msg.keys())
+    # print(decoded_msg.get_payload()[1])
+    if decoded_msg:
+        print('From:', decoded_msg['From'])
+        print('Subject:', decoded_msg['Subject'])
+        print('Body:', decoded_msg.get_payload())
+        print('-----------------------')
+    break
